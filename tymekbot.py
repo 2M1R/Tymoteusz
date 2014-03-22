d1514e52c8cd0dfa60fd63a0db3ad723f5a11c7f
Michał Maciej Różański <michal.rozanski@gmail.com>
2014-03-22 15:10:08 +0100

    simple commands system

--- tymekbot.py
+++ tymekbot.py
@@ -8,13 +8,12 @@ from pyquery import PyQuery as pq
 
 class TymoteuszBot(ClientXMPP):
 
-    def __init__(self, jid, password, room, nick):
+    def __init__(self, jid, password, nick):
         super(TymoteuszBot, self).__init__(jid, password)
 
         self.botname = 'Tymoteusz XMPP Bot'
-        self.botver = '0.2.30-dev'
+        self.botver = '0.2.50-dev'
 
-        self.room = room
         self.nick = nick
 
         #self.register_plugin('xep_0030')    #Disco
@@ -35,21 +34,48 @@ class TymoteuszBot(ClientXMPP):
 
 
         self.add_event_handler('session_start', self.start)
-        #self.add_event_handler('message', self.message)
+        self.add_event_handler('message', self.message)
         self.add_event_handler('groupchat_message', self.muc_message)
 
     def start(self, event):
         self.send_presence()
         self.get_roster()
-        self['xep_0045'].joinMUC(self.room,
-                                 self.nick,
-                                 wait=True)
 
     def message(self, msg):
-        pass
+        self.cmds(msg)
 
     def muc_message(self, msg):
         self.url_title(msg)
+
+    def cmds(self, msg):
+        if msg['body'].startswith('!'):
+            cmdsplit = msg['body'].split(None, 1)
+
+            if len(cmdsplit) == 2:
+                cmd, arg = cmdsplit
+            else:
+                cmd = cmdsplit[0]
+
+            if cmd == '!join':
+                self.joinChat(arg)
+            if cmd == '!leave':
+                self.leaveChat(msg['mucroom'])
+
+    def joinChat(self, room):
+        self['xep_0045'].joinMUC(room,
+                                 self.nick,
+                                 wait=True)
+        self.send_message(mto=room,
+                          mbody='Witam!',
+                          mtype='groupchat')
+
+    def leaveChat(self, room):
+        self.send_message(mto=room,
+                          mbody='Żegnam!',
+                          mtype='groupchat')
+        self['xep_0045'].leaveMUC(room,
+                                 self.nick,
+                                 msg='A teraz drogie dzieci poczałujcie bota w dupę...')
 
     def url_title(self, msg):
         for word in msg['body'].split():
@@ -60,18 +86,18 @@ class TymoteuszBot(ClientXMPP):
                     if url.is_('title'):
                         title = url('title').text()
                         self.send_message(mto=msg['mucroom'],
-                                          mbody="[URL] " + title,
+                                          mbody='[URL] ' + title,
                                           mtype='groupchat')
                 except:
                     self.send_message(mto=msg['mucroom'],
-                                      mbody="[URL] Nie można odczytać tytułu",
+                                      mbody='[URL] Nie można odczytać tytułu',
                                       mtype='groupchat')
 
 
 
 
 if __name__ == '__main__':
-    xmpp = TymoteuszBot(sys.argv[1], sys.argv[2], 'n00b.code()@conf.netlab.cz', 'Tymoteusz')
+    xmpp = TymoteuszBot(sys.argv[1], sys.argv[2], 'Tymoteusz-dev')
 
     if xmpp.connect():
         xmpp.process(block=True)
